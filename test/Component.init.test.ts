c8be030a3fa1ee80dabb98ffb4aee4a605dc8bf5
Willy Brauner <mail@willybrauner.com>
2022-11-04 09:43:00 +0100

    Remove unused wait

--- test/Component.init.test.ts
+++ test/Component.init.test.ts
@@ -1,7 +1,6 @@
 import { expect, it } from "vitest"
 import { Window } from "happy-dom"
 import { HomePageMock } from "./__mocks__/HomePageMock"
-import { wait } from "./helpers/wait"
 import { Component } from "../src"
 
 let window = new Window()
@@ -16,11 +15,10 @@ it("Extended class should be init properly.", async () => {
   const $root = document.querySelector(".HomePage") as any
   const props = { foo: "bar" }
   const homePage = new HomePage($root, props)
+
   expect(HomePage.attrName).toBe("HomePage")
   expect(homePage.$root).toBe($root)
   expect(homePage.name).toBe("HomePage")
   expect(homePage.props).toBe(props)
   expect(parseInt($root.getAttribute("data-component-id"))).toBe(0)
-  // wait next frame before next test because of mock
-  await wait(0)
 })
